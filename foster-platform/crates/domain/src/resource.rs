ae1c3770d5111e41fccb77e71ed6b9284bd2ae86
YIYOU KONG <597478495@qq.com>
2026-09-22 20:28:47 +0800

    feat: add resource pool domain types

---
+++ foster-platform/crates/domain/src/resource.rs
@@ -0,0 +1,35 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Serialize, Deserialize)]
+#[serde(rename_all = "SCREAMING_SNAKE_CASE")]
+pub enum ProviderStatus {
+    Active,
+    Disabled,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Serialize, Deserialize)]
+#[serde(rename_all = "SCREAMING_SNAKE_CASE")]
+pub enum FriendBindingStatus {
+    Verified,
+    Suspect,
+    Broken,
+    Disabled,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Serialize, Deserialize)]
+#[serde(rename_all = "SCREAMING_SNAKE_CASE")]
+pub enum ResourceCycleStatus {
+    Available,
+    Full,
+    Ended,
+    Disabled,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Serialize, Deserialize)]
+#[serde(rename_all = "SCREAMING_SNAKE_CASE")]
+pub enum ResourceAllocationStatus {
+    Reserved,
+    Confirmed,
+    Released,
+    Expired,
+}
