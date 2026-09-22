fa2277ee61e9696260eeaca2e08f43b72faafd2c
YIYOU KONG <597478495@qq.com>
2026-09-23 00:37:01 +0800

    style: rustfmt crates/server/src/main.rs

--- foster-platform/crates/server/src/main.rs
+++ foster-platform/crates/server/src/main.rs
@@ -9,8 +9,7 @@ use sqlx::mysql::MySqlPoolOptions;
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     tracing_subscriber::fmt()
         .with_env_filter(
-            tracing_subscriber::EnvFilter::try_from_default_env()
-                .unwrap_or_else(|_| "info".into()),
+            tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| "info".into()),
         )
         .init();
 
