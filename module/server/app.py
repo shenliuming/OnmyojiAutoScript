5fa5f7f1f1ad6aaa3a9dab3e6bc6ca7e8cf1d0f4
YIYOU KONG <597478495@qq.com>
2026-09-22 19:07:04 +0800

    feat: register OAS foster bridge router

--- module/server/app.py
+++ module/server/app.py
@@ -15,6 +15,7 @@ from module.logger import logger
 from module.server.home_router import home_app
 from module.server.script_router import script_app
 from module.server.tool_router import tool_app
+from module.foster_bridge.router import foster_app
 from module.server.setting import State
 from module.server.main_manager import mm
 from starlette.staticfiles import StaticFiles
@@ -44,6 +45,7 @@ app.add_middleware(
 app.include_router(home_app)
 app.include_router(script_app)
 app.include_router(tool_app)
+app.include_router(foster_app)
 
 annotator_static_dir = Path(__file__).resolve().parent / "web" / "annotator" / "static"
 if annotator_static_dir.exists():
